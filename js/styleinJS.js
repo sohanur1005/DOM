524470b236cb698137c907f84989dc01f493ab57
sohanur24059 <sohanurrahman24059@gmail.com>
2025-10-04 22:46:25 +0600

    send this

---
+++ js/styleinJS.js
@@ -0,0 +1,11 @@
+const sections=document.querySelectorAll('section');
+for(const section of sections )
+{
+    section.style.border='2px solid steelblue';
+    section.style.marginBottom='5px';
+    section.style.backgroundColor='lightgray';
+}
+
+
+const placesContainer=document.getElementById('place-container')
+placesContainer.style.backgroundColor='yellow';
